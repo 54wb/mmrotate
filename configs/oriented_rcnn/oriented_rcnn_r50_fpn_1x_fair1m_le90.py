a693accbff03b3e8b43c655caf4270e972df36b1
54wb <1225768798@qq.com>
2022-12-25 12:24:04 +0800

    test baseline using adamw optimizer

--- configs/oriented_rcnn/oriented_rcnn_r50_fpn_1x_fair1m_le90.py
+++ configs/oriented_rcnn/oriented_rcnn_r50_fpn_1x_fair1m_le90.py
@@ -143,4 +143,16 @@ data = dict(
     val=dict(version=angle_version),
     test=dict(version=angle_version))
 
-optimizer = dict(lr=0.005)
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+lr_config = dict(warmup_iters=1000, step=[8,11])
