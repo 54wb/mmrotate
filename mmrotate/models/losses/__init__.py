9956775de0884864b30d0a88e584edffc3d18130
54wb <1225768798@qq.com>
2022-12-27 22:33:00 +0800

    add arcface loss

--- mmrotate/models/losses/__init__.py
+++ mmrotate/models/losses/__init__.py
@@ -9,10 +9,11 @@ from .smooth_focal_loss import SmoothFocalLoss
 from .spatial_border_loss import SpatialBorderLoss
 from .label_smooth_loss import LabelSmoothLoss
 from .cross_entrypy_loss import CrossEntropyloss
+from .arcface_loss import ArcFaceLoss
 from .utils import (convert_to_one_hot, weight_reduce_loss)
 __all__ = [
     'GDLoss', 'GDLoss_v1', 'KFLoss', 'ConvexGIoULoss', 'BCConvexGIoULoss',
     'KLDRepPointsLoss', 'SmoothFocalLoss', 'RotatedIoULoss',
     'SpatialBorderLoss', 'LabelSmoothLoss', 'convert_to_one_hot', 'CrossEntropyloss',
-    'weight_reduce_loss'
+    'weight_reduce_loss', 'ArcFaceLoss'
 ]
