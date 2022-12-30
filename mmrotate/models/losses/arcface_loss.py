52e9d2d42f8e3bbfe67a915f67f159635c9b77a8
54wb <1225768798@qq.com>
2022-12-30 16:34:30 +0800

    fix arcface_loss

--- mmrotate/models/losses/arcface_loss.py
+++ mmrotate/models/losses/arcface_loss.py
@@ -9,36 +9,23 @@ from ..builder import ROTATED_LOSSES
 
 @ROTATED_LOSSES.register_module()
 class ArcFaceLoss(nn.Module):
-    def __init__(self, in_features, out_features, s=10.0, m=0.1, easy_margin=False):
+    def __init__(self, in_features, out_features, s=10.0, m=0.1):
         super(ArcFaceLoss, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.s = s
         self.m = m
-        self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
+        self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
         nn.init.xavier_uniform_(self.weight)
 
-        self.easy_margin = easy_margin
-        self.cos_m = math.cos(m)
-        self.sin_m = math.sin(m)
-        self.th = math.cos(pi - m)
-        self.mm = math.sin(pi - m)
     
-    def forward(self, input, label):
+    def forward(self, input):
         cosine = F.linear(F.normalize(input), F.normalize(self.weight))
-        phi = torch.cos(torch.acos(cosine) + self.m)
-        if self.easy_margin:
-            phi = torch.where(cosine > 0, phi, cosine)
-        else:
-            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
-        # --------------------------- convert label to one-hot ---------------------------
-        # one_hot = torch.zeros(cosine.size(), requires_grad=True, device='cuda')
-        one_hot = torch.zeros(cosine.size(), device=input.device)
-        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
-        # -------------torch.where(out_i = {x_i if condition_i else y_i) -------------
-        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)  # you can use torch.where if your torch.__version__ is 0.4
-        output *= self.s
-        # print(output)
+        theta = torch.acos(cosine)
+        top = torch.exp((torch.cos(theta + self.m)) * self.s)
+        top_ = torch.exp(torch.cos(theta) * self.s)
+        bottom = torch.sum(torch.exp(cosine * self.s), dim=1).view(-1,1)
+        output = (top / (bottom - top_ + top)) + 1e-10
 
         return output
 
