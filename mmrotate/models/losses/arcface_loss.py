7dbfbc5648c09467dbaeadbd30bd5815ef89919d
54wb <1225768798@qq.com>
2023-01-10 17:00:28 +0800

    fix arcface for margin

--- mmrotate/models/losses/arcface_loss.py
+++ mmrotate/models/losses/arcface_loss.py
@@ -1,4 +1,5 @@
 import math
+import copy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,7 +10,7 @@ from ..builder import ROTATED_LOSSES
 
 @ROTATED_LOSSES.register_module()
 class ArcFaceLoss(nn.Module):
-    def __init__(self, in_features, out_features, s=10.0, m=0.1):
+    def __init__(self, in_features, out_features, s=10.0, m=0.05):
         super(ArcFaceLoss, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -22,6 +23,16 @@ class ArcFaceLoss(nn.Module):
     def forward(self, input):
         cosine = F.linear(F.normalize(input), F.normalize(self.weight))
         theta = torch.acos(cosine)
+        # off = theta.clone()
+        # off = off + self.m
+        # off[:,8] = off[:,8] - self.m + 0.005
+        # off[:,5] = off[:,5] - self.m + 0.005
+        # off[:,6] = off[:,6] - self.m + 0.005
+        # off[:,11] = off[:,11] - self.m + 0.005
+        # off[:,12] = off[:,12] - self.m + 0.005
+        # off[:,16] = off[:,16] - self.m + 0.005
+        # off[:,28] = off[:,28] - self.m + 0.005
+        # off[:,34] = off[:,34] - self.m + 0.005
         top = torch.exp((torch.cos(theta + self.m)) * self.s)
         top_ = torch.exp(torch.cos(theta) * self.s)
         bottom = torch.sum(torch.exp(cosine * self.s), dim=1).view(-1,1)
