09755cc1c9b67714c233a68bb1c00fd8050ad70c
54wb <1225768798@qq.com>
2023-03-09 12:06:44 +0800

    add van backbone

--- mmrotate/models/backbones/__init__.py
+++ mmrotate/models/backbones/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .base_backbone import BaseBackbone
 from .re_resnet import ReResNet
 from .convnext import ConvNeXt
 from .detectors_swin import DetectoRS_SwinTransformer
-
-__all__ = ['ReResNet', 'ConvNeXt', 'DetectoRS_SwinTransformer']
+from .van import VAN
+__all__ = ['BaseBackbone', 'ReResNet', 'ConvNeXt', 'DetectoRS_SwinTransformer', 'VAN']
