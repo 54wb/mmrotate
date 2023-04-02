df31aba5543dc33f51f4663cbf817dc86f427226
54wb <1225768798@qq.com>
2023-04-02 21:54:18 +0800

    fix name

--- mmrotate/models/roi_heads/cls_heads/fine_grained_cls_head.py
+++ mmrotate/models/roi_heads/cls_heads/fine_grained_cls_head.py
@@ -64,7 +64,7 @@ class FineClsHead(BaseModule):
         self.fp16_enabled = False
 
         self.loss_cls = build_loss(loss_cls)
-        if self.loss_arcfacec is not None:
+        if self.loss_arcface is not None:
             self.loss_arcface = build_loss(loss_arcface)
         
         self.num_shared_fcs = num_shared_fcs
