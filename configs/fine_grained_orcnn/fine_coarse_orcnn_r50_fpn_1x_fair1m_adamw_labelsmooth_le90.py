d381a71621edddd14ab1ef3c254429ce74efce87
54wb <1225768798@qq.com>
2023-04-02 21:52:51 +0800

    change label-smooth for fine-coarse configs name

--- configs/fine_grained_orcnn/fine_coarse_orcnn_r50_fpn_1x_fair1m_adamw_labelsmooth_le90.py
+++ configs/fine_grained_orcnn/fine_coarse_orcnn_r50_fpn_1x_fair1m_adamw_labelsmooth_le90.py
@@ -77,7 +77,7 @@ model = dict(
             num_classes=37,
             reg_class_agnostic=True,
             loss_cls=dict(
-                type='LabelSmoothLoss', label_smooth_val=0.1, mode='original'))),
+                type='LabelSmoothLoss', num_classes = 38, label_smooth_val=0.01))),
     train_cfg=dict(
         rpn=dict(
             assigner=dict(
@@ -152,4 +152,16 @@ data = dict(
     val=dict(version=angle_version),
     test=dict(version=angle_version))
 
-optimizer = dict(lr=0.005)
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+lr_config = dict(warmup_iters=1000, step=[8,11])
